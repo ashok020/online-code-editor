c69c448fec5b47543abbf02426e0bad9d0162f4c
ashok020 <ashokshrawan20032001@gmail.com>
2021-12-28 13:38:51 +0530

    commit

---
+++ lib/lang_default/cpp.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<=n:i>=n;k<n?i++:i--)
+
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pll>		vpll;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+
+void solve()
+{
+    
+}
+
+void getTestCases()
+{
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        getTestCases();
+    }
+}
